fc7c60a85805544c292d979d2fa6521a5b29d4a5
Jbns89 <Jbns89@gmail.com>
2014-09-23 11:42:47 -0500

    only using this to lower case any filename

--- public/classes/address_data_store.php
+++ public/classes/address_data_store.php
@@ -1,41 +1,16 @@
 <?php
 
-class AddressDataStore{
-    public $filename = '';
-    
-    public function __construct($files = FILE) {
-        $this->filename = $files;
-    }
-    
-    public function read_address_book() {
+// TODO: require Filestore class
 
-        if (filesize($this->filename) == 0) {
-            $array = [];
-        }
-        
-        else {
-            $handle = fopen($this->filename, 'r');
-            
-            while(!feof($handle)) {
-                $row = fgetcsv($handle);
-                
-                if (!empty($row)) {
-                    $array[] = $row;
-                }
-            }
-            fclose($handle);
-        }
-        
-        return $array;
+ class AddressDataStore extends Filestore 
+ {
+    function __construct ($files = '')
+    {
+        //this is overwriting the parent __construct
+        //so any file names are automatically set to lowercase
+        parent::__construct(strtolower($files));
     }
-    public function write_address_book($array){
-        $handle = fopen($this->filename, 'w');
-        foreach ($array as $rows) {
-            fputcsv($handle, $rows);
-        }
-        fclose($handle);
-        return $array;
-    }
-}
+    
+ }
 
 ?>
