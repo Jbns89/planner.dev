0736777f3567638f99251df0fba53de02241960e
Jbns89 <Jbns89@gmail.com>
2014-09-24 16:26:57 -0500

    using databases to store info instead of csv and txt files

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,54 +1,60 @@
 <?php 
 
-    // var_dump($_POST); 
-    // var_dump($_GET);
-    // var_dump($_FILES);
-    define('FILE', 'list_items.txt');
+// var_dump($_POST); 
+// var_dump($_GET);
+// var_dump($_FILES);
+
+require '../dbconn_todo.php';
+
+class InvalidInputException extends Exception { }
+
+if (isset($_POST['Todo'])) 
+{
+    if (!empty($_POST['Todo']) && strlen($_POST['Todo']) < 241)
+    {
+        $stmt = $dbc->prepare("INSERT INTO todo_items (item_name) VALUES (:item)");
+        $stmt->bindValue(':item', $_POST['Todo'], PDO::PARAM_STR);
+        $stmt->execute();
+    }
+
+}
+
+if (isset($_GET['remove'])) 
+{
+    $keyRemoved = $_GET['remove'];
+    $stmt = $dbc->prepare("DELETE FROM todo_items WHERE id = :id");
+    $stmt->bindValue(':id', $keyRemoved, PDO::PARAM_INT);
+    $stmt->execute();
     
-    require_once ('../inc/filestore.php');
+}
+
+if (count($_FILES) > 0 && $_FILES['uploaded']['error'] == UPLOAD_ERR_OK) 
+{
+    $upload_dir = '/vagrant/sites/planner.dev/public/uploads/';
     
-    //Here you need to put the file you want
-    //because there isn't a default set in the
-    // filestore class 
-    $lists = new Filestore(FILE);
-    $items = $lists->read();
+    $filename = basename($_FILES['uploaded']['name']);
+    
+    $saved_filename = $upload_dir . $filename;
+    
+    move_uploaded_file($_FILES['uploaded']['tmp_name'], $saved_filename);
+    
+    
+    //open_file is finding the new items by 
+    //$filename being added onto the the uploads/ path
+    $itemsNew = $lists->read("uploads/" . $filename);
+    
+    
+    //need to create a new variable so that once the arrays merge
+    //they'll be saved and over written the items array 
+    $items = array_merge($items, $itemsNew);
+    
+    $lists->write($items, FILE);
+}
 
+$stmt = $dbc->prepare("SELECT * FROM todo_items");
+$stmt->execute();
+$items = $stmt->fetchall(PDO::FETCH_ASSOC);
 
-
-    if (isset($_POST['Todo'])) 
-    {
-        $items[] = $_POST['Todo'];
-        $lists->write($items);
-    }
-    if (isset($_GET['remove'])) 
-    {
-        $keyRemoved = $_GET['remove'];
-        unset($items[$keyRemoved]);
-        $items = array_values($items);
-        $lists->write($items);
-    }
-    if (count($_FILES) > 0 && $_FILES['uploaded']['error'] == UPLOAD_ERR_OK) 
-    {
-        $upload_dir = '/vagrant/sites/planner.dev/public/uploads/';
-        
-        $filename = basename($_FILES['uploaded']['name']);
-        
-        $saved_filename = $upload_dir . $filename;
-        
-        move_uploaded_file($_FILES['uploaded']['tmp_name'], $saved_filename);
-        
-        
-        //open_file is finding the new items by 
-        //$filename being added onto the the uploads/ path
-        $itemsNew = $lists->read("uploads/" . $filename);
-        
-        
-        //need to create a new variable so that once the arrays merge
-        //they'll be saved and over written the items array 
-        $items = array_merge($items, $itemsNew);
-        
-        $lists->write($items, FILE);
-    }
 ?>
 
 <!doctype html>
@@ -60,10 +66,10 @@
     <body>
         <h2>To Do List for today:</h2>
         <ul>
-            <?php foreach ($items as $key => $value): ?>
-                    <!-- Don't need to have quotes around $key
-                    you will cause items to not be removed--> 
-                    <li><?= "$value"?><a href=?remove=<?=$key?>>Complete</a></li>
+            <?php foreach ($items as $item ): ?>
+                <!-- Don't need to have quotes around $key
+                you will cause items to not be removed--> 
+                <li><?= $item['item_name']?><a href=?remove=<?=$item['id']?>>Complete</a></li>
             <?php endforeach; ?>
         </ul>
         <h2>Enter your to do list item</h2>
@@ -71,6 +77,7 @@
             <label for="Todo"></label>
                 <input id="Todo" name="Todo" type="text" placeholder="Enter todo item">
             <button type="Submit">Add</button>
+
         </form>
         <form clas="uploads" method="POST" enctype="multipart/form-data" action="todo_list.php">
             <p>
